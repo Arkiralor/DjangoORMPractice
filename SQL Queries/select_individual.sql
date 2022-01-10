7c93302cafb4362f2d0575754b42ab9d19b25f32
Arkiralor <pritho11335@gmail.com>
2022-01-10 11:21:11 +0530

    sql queries.

--- SQL Queries/select_individual.sql
+++ SQL Queries/select_individual.sql
@@ -1,3 +1,5 @@
 select * from db_api_pokemon where pokedex_id = 88;
 select * from db_api_stat where pokedex_id_id = 259;
 select * from db_api_multiplier where pokedex_id_id = 259;
+
+select db_api_pokemon.name, db_api_pokemon.pokedex_id from db_api_pokemon where is_legendary = true;
