73b5e887528d9a450eb33c51b473474a2c0fbfb1
Arkiralor <prithoo11335@gmail.com>
2022-01-15 19:43:13 +0530

    starting auth.

--- mainproject/blogapp/urls.py
+++ mainproject/blogapp/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserView, BlogPostView
+from .views import UserView, BlogView, BlogPostView
 
 router = DefaultRouter()
-router.register('users', UserView)
-router.register('posts', BlogPostView)
+router.register('blog/users', UserView)
+# router.register('posts', BlogPostView)
+# router.register('posts', BlogView.as_view({"get": "list","post": "create"}), basename='blog_posts')
+# router.register('posts', BlogView.as_view(), basename='blog_posts')
 
 
 urlpatterns = [
-    path('blog/', include(router.urls))
+    path('', include(router.urls)),
+    path('blog/posts', BlogView.as_view(), name='blog_posts'),
+    path('blog/posts/<int:id>', BlogPostView.as_view(), name='ind_blog_posts')
 ]
--- mainproject/blogapp/views.py
+++ mainproject/blogapp/views.py
@@ -4,6 +4,8 @@ from .serializers import *
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 
 # Create your views here.
@@ -15,12 +17,105 @@ class UserView(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_field = 'id'
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAdminUser]
 
 
-class BlogPostView(ModelViewSet):
+# class BlogPostView(ModelViewSet):
+#     '''
+#     API Model View for all blog posts.
+#     '''
+#     queryset = Blog.objects.all()
+#     serializer_class = BlogSerializer
+#     lookup_field = 'id'
+
+class BlogView(APIView):
     '''
-    API Model View for all blog posts.
+    View to get all blog-posts and post new blog-post.
     '''
-    queryset = Blog.objects.all()
-    serializer_class = BlogSerializer
-    lookup_field = 'id'
+
+    def get(self, request):
+        try:
+            queryset = Blog.objects.all()
+            blog_posts = BlogSerializer(queryset, many=True)
+        except Blog.DoesNotExist:
+            return Response(
+                {
+                    "error": "No posts in database."
+                },
+                status=status.HTTP_204_NO_CONTENT
+            )
+
+        return Response(
+            blog_posts.data,
+            status=status.HTTP_302_FOUND
+        )
+
+    def post(self, request):
+        try:
+            data = BlogSerializer(data=request.data)
+        except Exception as err:
+            return Response(
+                {
+                    "error": str(err)
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if data.is_valid():
+            data.save()
+            return Response(
+                data.data,
+                status=status.HTTP_201_CREATED
+            )
+        else:
+            return Response(
+                {
+                    "error": str(data.errors)
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+class BlogPostView(APIView):
+    '''
+    Methods for individual blog posts.
+    '''
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, id: int):
+        try:
+            query = Blog.objects.get(pk=id)
+            blog_post = BlogSerializer(query)
+        except Blog.DoesNotExist:
+            return Response({"error": f"No content with ID: {id}"}, status=status.HTTP_204_NO_CONTENT)
+        return Response(blog_post.data, status=status.HTTP_302_FOUND)
+
+    def put(self, request, id: int):
+        blog_object = Blog.objects.get(pk=id)
+        update = request.data
+
+        serialized = BlogSerializer(blog_object, data=update)
+        if serialized.is_valid():
+            serialized.save()
+            return Response(
+                serialized.data,
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {
+                    "error": str(serialized.errors)
+                },
+                status=status.HTTP_406_NOT_ACCEPTABLE
+            )
+
+    def delete(self, request, id):
+        blog_post = Blog.objects.get(pk=id)
+        serialized = BlogSerializer(blog_post)
+        blog_post.delete()
+        return Response(
+            serialized.data,
+            status=status.HTTP_410_GONE
+        )
