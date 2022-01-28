192aeab01671dbfbe113d39b068f7fce376bee94
Arkiralor <pritho11335@gmail.com>
2022-01-28 13:11:02 +0530

    removed non-essential code.

---
+++ SQL Queries/drop_table_001.sql
@@ -0,0 +1,21 @@
+drop table auth_group cascade;
+drop table auth_group_permissions;
+drop table auth_permissions;
+drop table auth_user cascade;
+drop table auth_user_groups;
+drop table auth_user_user_permissions;
+drop table db_api_multiplier;
+drop table db_api_pokemon cascade;
+drop table db_api_stat;
+drop table django_admin_log;
+drop table django_content_type;
+drop table django_migrations;
+drop table django_session;
+drop table messenger_user;
+
+
+
+
+
+
+
